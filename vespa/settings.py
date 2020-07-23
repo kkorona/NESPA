f68c91de834a3c1db62af1c2f144d767dcba814c
kkorona <kkorona@github.com>
2020-07-23 19:16:19 +0900

    solving static file dependency

--- vespa/settings.py
+++ vespa/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import mimetypes
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,4 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+mimetypes.add_type('text/html','.html',True)
+mimetypes.add_type('text/html','.css',True)
+mimetypes.add_type('text/css','.css',True)
--- vespa/urls.py
+++ vespa/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 import dev2020.views
 
 urlpatterns = [
