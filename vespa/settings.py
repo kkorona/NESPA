96a55a33920c352d71a5965116eade28d80618ff
kkorona <kkorona@github.com>
2020-07-24 17:47:45 +0900

    solved static problem

--- vespa/settings.py
+++ vespa/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static/"),
 ]
 
-STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 mimetypes.add_type('text/html','.html',True)
 mimetypes.add_type('text/html','.css',True)
