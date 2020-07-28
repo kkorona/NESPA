73f8fcc00b3535e29ac09c80d5fcdca0885d0025
kkorona <high1uck@pusan.ac.kr>
2020-07-28 14:51:25 +0900

    accounts

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('signup/', view.signup, name='signup'),
+    path('login/', view.login, name='login'),
+    path('logout/', view.logout, name='logout'),
+]
+
+
+
+
