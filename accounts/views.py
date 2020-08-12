1832f74a828be4b359e77367b9d24056749da4dd
kkorona <high1uck@pusan.ac.kr>
2020-08-12 18:35:13 +0900

    walla

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 
@@ -12,9 +12,11 @@ def signup(request):
     return render(request, 'accounts/signup.html')
 
 def login(request):
+    if request.method == "GET":
+            return render(request, 'login.html')
     if request.method == "POST":
-        username = request.POST["user_name"]
-        password = request.POST["password"]
+        username = request.POST.get("user_name")
+        password = request.POST.get("password")
         user = auth.authenticate(request, username=username, password=password)
         if user is not None:
             auth.login(request, user)
