a3ed770be8d598237b8b1b1217fe06b0b1859148
kkorona <high1uck@pusan.ac.kr>
2020-08-14 18:15:35 +0900

    sleep

--- assignment/views.py
+++ assignment/views.py
@@ -4,4 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def submission(request):
-    return render(request, 'submission.html')
+    if request.method == "GET":
+        return render(request, 'submission.html')
+        
+    if request.method == "POST":
+        prob_ID = request.POST.get('',None)
+        source
