4f9cea71e374bcd274403043bf4ae215ed8c249e
kkorona <high1uck@pusan.ac.kr>
2020-08-28 17:22:42 +0900

    htresponsenotfound

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponseNotFound
 from django.contrib.auth.hashers import make_password, check_password
 from .models import vespaUser
 import re
