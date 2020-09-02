e0ab24ceef6a0e38c4f58f8a00aa6db13ee02cec
kkorona <high1uck@pusan.ac.kr>
2020-09-02 17:07:38 +0900

    ordering

--- QnA/models.py
+++ QnA/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
         verbose_name = 'post'
         verbose_name_plural = 'posts'
         db_table = 'qna_posts'
-        ordering = ('-mod_date',)
+        ordering = ('-post_date',)
 
     def __str__(self):
         return self.title
