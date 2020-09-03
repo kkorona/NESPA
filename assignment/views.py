41a12568b1094de503e8d5fbe43fecc1023d968d
kkorona <high1uck@pusan.ac.kr>
2020-09-03 17:55:29 +0900

    okborn

--- assignment/views.py
+++ assignment/views.py
@@ -9,6 +9,7 @@ def submission(request):
         
     if request.method == "POST":
         prob_ID = request.POST.get('',None)
+        # https://simpleisbetterthancomplex.com/tutorial/2016/08/01/how-to-upload-files-with-django.html
         
 def assignment_list(request):
     if request.method == "GET":
