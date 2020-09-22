57b76fb1175659567d6fd0746a189e707e5f85fc
kkorona <high1uck@pusan.ac.kr>
2020-09-22 12:15:02 +0900

    adf

--- forum/models.py
+++ forum/models.py
@@ -16,7 +16,7 @@ class Post(models.Model):
         verbose_name = 'post'
         verbose_name_plural = 'posts'
         db_table = 'forum_posts'
-        ordering = ('-pub_date',)
+        ordering = ('pub_date',)
 
     def __str__(self):
         return self.title
