d9eab878e7fd567e5ea2c6303a318affeeedff39
rubinstory@naver.com <rubinstory@naver.com>
2021-07-25 04:55:07 +0900

    댓글 삭제여부가 POST 리스트에 반영되게 했습니다

--- QnA/models.py
+++ QnA/models.py
@@ -39,9 +39,12 @@ class Post(models.Model):
         self.save()
         return ''
 
+    def comments_count(self):
+        return len(self.comment_set.filter(deleted=False))
+
 class Comment(models.Model):
     parent = models.ForeignKey(Post, on_delete=models.CASCADE)
-    retweet = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+    retweet = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
     deleted = models.BooleanField(default=False)
     author = models.CharField(max_length=50)
     text = models.TextField()
