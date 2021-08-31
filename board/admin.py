2a34a5679e17835fae11d7d45ba919266411c7d4
kkorona <root@localhost.localdomain>
2021-08-31 17:38:35 +0900

    공지사항 댓글 버그 수정

--- board/admin.py
+++ board/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from board.models import Post, Attach
+from board.models import Post, Attach, Comment
 
 # Register your models here.
 admin.autodiscover()
@@ -10,6 +10,12 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'author', 'pub_date', 'mod_date')
     list_filter = ('mod_date',)
     search_fields = ('title', 'author', 'content')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'text', 'pub_date', 'mod_date')
+    list_filter = ('pub_date',)
+    search_fields = ('title', 'author')
 
 @admin.register(Attach)
 class AttachAdmin(admin.ModelAdmin):
